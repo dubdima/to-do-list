1debc18bdc57731b1d759c27b37fef32e9edf8f8
dubdima <128041506+dubdima@users.noreply.github.com>
2025-06-09 15:55:15 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Раздача статических файлов
 app.use(express.static(path.join(__dirname)));
 
-// Все запросы перенаправляем на index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
